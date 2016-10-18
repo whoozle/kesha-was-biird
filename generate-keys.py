611a2e912c706c4e10a717b01cba46956f0d0e0f
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-18 23:08:19 +0100

    use only three notes used in main theme

--- generate-keys.py
+++ generate-keys.py
@@ -8,5 +8,5 @@ freqs = [313.96, 348.83, 367.92, 418.60, 470.93, 490.55, 545.08]
 
 print ": music_keys"
 
-for freq in freqs:
-	generate(pulse(freq * 2))
+for freq in [6, 0, 2]:
+	generate(pulse(freqs[freq] * 2))
