c553c183e18e746e579a018d057e0fa463b5ac32
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-18 21:05:55 +0100

    added pykesha.audio, use same sources for sound generation

---
+++ generate-keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+from pykesha.audio import *
+
+#Eb F Gb Ab Bb Cb Db
+#http://www.phy.mtu.edu/~suits/scales.html
+freqs = [313.96, 348.83, 367.92, 418.60, 470.93, 490.55, 545.08]
+
+print ": music_keys"
+
+for freq in freqs:
+	generate(pulse(freq))
---
+++ pykesha/audio.py
@@ -0,0 +1,40 @@
+FREQ = 4000
+from math import sin, pi, floor
+
+def osc(t, freq):
+	return sin(t * 2 * pi * freq)
+
+def pulse(freq):
+	buf = []
+	byte = 0
+	bit = 0
+	for i in xrange(0, 128):
+		v = osc(i * 1.0 / FREQ, freq)
+		buf.append(v)
+	return buf
+
+def generate(tone):
+	byte = 0
+	bit = 0
+	qe = 0
+	for i in xrange(0, 128):
+		x = tone[i]
+		if x >= qe:
+			byte |= (0x80 >> bit)
+			y = 1
+		else:
+			y = -1
+		qe = y - x + qe
+
+		bit += 1
+		if bit == 8:
+			print "0x%02x" %byte,
+			bit = 0
+			byte = 0
+	print
+
+def mix(a, b):
+	c = []
+	for i in xrange(0, 128):
+		c.append(a[i] + b[i])
+	return c
