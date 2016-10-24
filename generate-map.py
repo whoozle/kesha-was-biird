b850123cf05d3b6ec46598d1ee6c5d09de497c0e
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-24 19:51:49 +0100

    added lab location, removed old professor lines

--- generate-map.py
+++ generate-map.py
@@ -7,15 +7,14 @@ parser = argparse.ArgumentParser(description='generate maps')
 parser.add_argument('prefix', help='target directory')
 args = parser.parse_args()
 
-lab_ruins = Location('DEAD LAB')
+lab = Location('LAB INTERIOR')
+lab_def = State('Lab refused to die easily,\nsome unrecognizeable panels\nare still flashing with leds')
+lab.add_state('default', lab_def)
+
+lab_ruins = Location('DEAD LAB ENTRANCE')
 lab_ruins_def = State("Black mouth of dead lab\nlays before you")
 lab_ruins_def.add_action(Action('Go outside', go(lab_ruins)))
 lab_ruins.add_state('default', lab_ruins_def)
-
-#vault.text("Proffesor woke up.")
-#vault.text("It's been three years")
-#vault.text("after that tragic night.")
-#vault.text("But memories, they still haunt him")
 
 vault = Location('VAULT')
 
@@ -26,6 +25,7 @@ vault_def.add_action(Action('Stay inside', rest()))
 vault.add_state('default', vault_def)
 
 generator = Generator()
+generator.visit(lab)
 generator.visit(vault)
 generator.visit(lab_ruins)
 
