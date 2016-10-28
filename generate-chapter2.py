447bc4e6c03f14c876a9e2b6e7349e7513050adc
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-28 09:16:54 +0100

    added glitch location

--- generate-chapter2.py
+++ generate-chapter2.py
@@ -7,5 +7,9 @@ parser = argparse.ArgumentParser(description='generate maps')
 parser.add_argument('prefix', help='target directory')
 args = parser.parse_args()
 
+glitch = Location('GLITCH', 'FINALLY WE MET\nWhat do you think you\ndo in this CHIP8 realm?')
+glitch.add_action(Action('Nothing'))
+
 generator = Generator()
+generator.visit(glitch)
 generator.generate(args.prefix, 'chapter2')
