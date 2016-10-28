cbcf4aba8ab4ee25be261d292595e04a156aa9cf
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-28 22:24:04 +0100

    added rummage-thongs logic

--- generate-chapter1.py
+++ generate-chapter1.py
@@ -9,6 +9,7 @@ args = parser.parse_args()
 
 lab = Location('LAB INTERIOR', 'Lab refused to die easily,\nsome unrecognizeable panels\nare still flashing with leds')
 lab.add_action(Action('Look into broken compartment', go('isotope')))
+lab.add_action(Action('Return to lab\'s entrance', go('dead_lab_entrance')))
 
 f_ray = Location('F-RAY ISOTOPE', """F-ray isotope glowing with
 green light, which is physically
@@ -21,14 +22,14 @@ f_ray.add_action(Action('Continue searching', go(lab)))
 f_ray.add_action(Action('Pick isotope with tongs', go(lab), predicate = test('chapter1_got_tongs', 1)))
 
 chav = Location('CHAV', 'Hey you, have you seen\nthe sign there?')
-chav.add_action(Action("Yes, I have"))
-chav.add_action(Action("Do nothing"))
+chav.add_action(Action("Yes, I have", banner('tile_chav_data', 'text_chav_thanks'), set_flag('chapter1_got_tongs', 1), go('dead_lab_entrance')))
+chav.add_action(Action("Do nothing", banner('tile_chav_data', 'text_chav_thanks'), set_flag('chapter1_got_tongs', 1), go('dead_lab_entrance')))
 chav.add_action(Action("Run", go('dead lab entrance')))
 
 lab_ruins = Location('DEAD LAB ENTRANCE', "Black mouth of dead lab\nlays before you. Sign says\nRummaging punished by death.")
 lab_ruins.add_action(Action('Enter dark corridor', go(lab)))
 lab_ruins.add_action(Action('Go outside', go(lab_ruins)))
-lab_ruins.add_action(Action('Rummage through rubbish', banner('tile_chav_data', 'text_chav_greeting'), go(chav)))
+lab_ruins.add_action(Action('Rummage through rubbish', banner('tile_chav_data', 'text_chav_greeting'), go(chav), predicate=test('chapter1_got_tongs', 0)))
 
 vault_bed = Location('BED', "Professor lays in bed sleepless\nHe keeps thinking on his\nnew invention")
 vault_bed.add_action(Action('Get out of bed', go('vault')))
