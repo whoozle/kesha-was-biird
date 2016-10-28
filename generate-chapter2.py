a00eb00d27cb1772a823df64c6878637b439dbed
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-28 11:07:42 +0100

    added glitch ending of chapter 2

--- generate-chapter2.py
+++ generate-chapter2.py
@@ -7,8 +7,9 @@ parser = argparse.ArgumentParser(description='generate maps')
 parser.add_argument('prefix', help='target directory')
 args = parser.parse_args()
 
-glitch = Location('GLITCH', "")
-glitch.add_action(Action('Nothing'))
+glitch = Location('GLITCH', "           P A U S I N G\n       S I M U L A T I O N\n\nHello, nice to see you again\nDo you remember me?")
+glitch.add_action(Action('Yes'))
+glitch.add_action(Action('Not really'))
 
 generator = Generator()
 generator.visit(glitch)
