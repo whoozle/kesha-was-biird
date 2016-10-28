713bc5ed57720b468b0dbcc279005f6749a1eaf6
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-28 10:56:24 +0100

    removed junk from chapter2

--- generate-chapter2.py
+++ generate-chapter2.py
@@ -7,11 +7,8 @@ parser = argparse.ArgumentParser(description='generate maps')
 parser.add_argument('prefix', help='target directory')
 args = parser.parse_args()
 
-glitch = Location('GLITCH', 'FINALLY WE MET\nWhat do you think you\ndo in this CHIP8 realm?')
+glitch = Location('GLITCH', "")
 glitch.add_action(Action('Nothing'))
-
-glitch2 = Location('GLITCH', "No. You keep playing this goddamn game!", id='glitch2')
-glitch2.add_action(Action('Stop'))
 
 generator = Generator()
 generator.visit(glitch)
